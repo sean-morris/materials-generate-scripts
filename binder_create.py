314e09bcfa0140cb36f579848527779e5dad8f63
sean-morris <sean.smorris@gmail.com>
2024-06-07 12:19:03 -0700

    Fixed Jupyterlite and Binder

--- binder_create.py
+++ binder_create.py
@@ -4,23 +4,20 @@ from util import process_ipynb, strip_unnecessary_keys
 
 
 def binderize(a_args, local_notebooks_folder):
-    root_copy_path = f"{os.getcwd()}/{local_notebooks_folder}_jupyterlite"
-    local_path = f"{local_notebooks_folder}/{a_args['assign_type']}/{a_args['file_no_ext']}"
-    assign_path = f"{os.getcwd()}/{local_path}"
+    root_copy_path = f"{os.getcwd()}/{local_notebooks_folder}_binder"
+    rel_path = f"{a_args['assign_type']}/{a_args['file_no_ext']}"
     file_name = f"{a_args['file_no_ext']}.ipynb"
-    file_path = os.path.join(assign_path, file_name)
-    for n in ["student", "autograder"]:
-        rel_path = "/".join(file_path.split("/")[7:-1])
-        copy_from = "/".join(file_path.split("/")[:-1])
-        copy_to = os.path.join(root_copy_path, rel_path)
-        copy_to_file = os.path.join(copy_to, file_name)
-        shutil.copytree(copy_from, copy_to, dirs_exist_ok=True)
-        shutil.copyfile(file_path, copy_to_file)
-        insert_headers = [
-            "# The pip install can take a minute\n",
-            f"%pip install -q otter-grader=={a_args['otter_version']}\n",
-            "%pip install -q datascience==0.17.5\n"
-        ]
-        process_ipynb(copy_to_file, insert_headers)
-        strip_unnecessary_keys(copy_to_file)
-    print(f"Binder: {local_path} Created")
+    copy_to = os.path.join(root_copy_path, rel_path)
+    copy_to_file = os.path.join(copy_to, file_name)
+    copy_from = f"{local_notebooks_folder}/{a_args['assign_type']}/{a_args['file_no_ext']}/student"
+
+    shutil.copytree(copy_from, copy_to, dirs_exist_ok=True)
+
+    insert_headers = [
+        "# The pip install can take a minute\n",
+        f"%pip install -q otter-grader=={a_args['otter_version']}\n",
+        "%pip install -q datascience==0.17.5\n"
+    ]
+    process_ipynb(copy_to_file, insert_headers)
+    strip_unnecessary_keys(copy_to_file)
+    print(f"Binder: {copy_to} Created")
--- jupyterlite_create.py
+++ jupyterlite_create.py
@@ -24,25 +24,21 @@ def replace_jl_otter_declare(file_path, name):
 
 def jupyterlite(a_args, local_notebooks_folder):
     root_copy_path = f"{os.getcwd()}/{local_notebooks_folder}_jupyterlite"
-    local_path = f"{local_notebooks_folder}/{a_args['assign_type']}/{a_args['file_no_ext']}"
-    assign_path = f"{os.getcwd()}/{local_path}"
+    rel_path = f"{a_args['assign_type']}/{a_args['file_no_ext']}"
     file_name = f"{a_args['file_no_ext']}.ipynb"
-    file_path = os.path.join(assign_path, file_name)
-    for n in ["student", "autograder"]:
-        user_assign_path = f"{assign_path}/{n}"
-        rel_path = f"{a_args['assign_type']}/{a_args['file_no_ext']}/{n}"
-        copy_from = user_assign_path
-        copy_to = os.path.join(root_copy_path, rel_path)
-        copy_to_file = os.path.join(copy_to, file_name)
-        shutil.copytree(copy_from, copy_to, dirs_exist_ok=True)
-        shutil.copyfile(file_path, copy_to_file)
-        insert_headers = [
-            "# The pip install can take a minute\n",
-            f"%pip install -q urllib3<2.0 otter-grader=={a_args['otter_version']} datascience ipywidgets\n",
-            "import pyodide_http\n",
-            "pyodide_http.patch_all()\n"
-        ]
-        process_ipynb(copy_to_file, insert_headers)
-        strip_unnecessary_keys(copy_to_file)
-        replace_jl_otter_declare(copy_to_file, file_name)
-    print(f"JupyterLite: {local_path}/ Created")
+    copy_to = os.path.join(root_copy_path, rel_path)
+    copy_to_file = os.path.join(copy_to, file_name)
+    copy_from = f"{local_notebooks_folder}/{a_args['assign_type']}/{a_args['file_no_ext']}/student"
+
+    shutil.copytree(copy_from, copy_to, dirs_exist_ok=True)
+
+    insert_headers = [
+        "# The pip install can take a minute\n",
+        f"%pip install -q urllib3<2.0 otter-grader=={a_args['otter_version']} datascience ipywidgets\n",
+        "import pyodide_http\n",
+        "pyodide_http.patch_all()\n"
+    ]
+    process_ipynb(copy_to_file, insert_headers)
+    strip_unnecessary_keys(copy_to_file)
+    replace_jl_otter_declare(copy_to_file, file_name)
+    print(f"JupyterLite: {copy_to}/ Created")
